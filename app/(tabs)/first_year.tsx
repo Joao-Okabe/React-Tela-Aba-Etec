c22b7a84ef90bbeced38f19b16bb57a7c9efd448
Joao-Okabe <joao.okae.berghem@gmail.com>
2026-05-07 00:42:05 -0300

    first_year.tsx completo e finalizado

--- app/(tabs)/first_year.tsx
+++ app/(tabs)/first_year.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
@@ -6,12 +6,47 @@ export default function HomeScreen() {
       style={styles.fundo}
       contentContainerStyle={styles.Container}
     >
-      <Text 
-        style={styles.title}
+      <View
+        style={[styles.titleBox]}
       >
-        Primeiro Ano
-      </Text>
+        <Text 
+          style={[styles.title, styles.whiteText]}
+        >
+          Primeiro Ano
+        </Text>
+      </View>
       
+      <View
+        style={[styles.subtitleBox]}
+      >
+        <Text
+          style={[styles.subtitle, styles.whiteText]}
+        >
+          Auxiliar de Informática
+        </Text>
+        <Text
+          style={[styles.subtitle, styles.whiteText]}
+        >
+          para Internet
+        </Text>
+      </View>
+      <View
+        style={styles.textBox}
+      >
+        <Image
+          source={require('../../assets/images/html_css_js.png')}
+          style={{
+            width: 100,
+            aspectRatio: 0.8,
+          }}
+          resizeMode="contain"
+        />
+        <Text 
+          style={[styles.subtitle, styles.whiteText]}
+        >
+          Estudo e programação Front-End
+        </Text>
+      </View>
     </ScrollView>
   );
 }
@@ -23,11 +58,47 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   fundo: {
-    backgroundColor: 'white'
+    backgroundColor: '#392F48'
   },
   title: {
     fontSize: 40,
-    marginTop: 40,
-    marginBottom: 80,
+  },
+  titleBox: {
+    width: '80%',
+    height: 150,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: '#2c1c44',
+    marginTop: 30,
+    backgroundColor: '#31214a',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  subtitle: {
+    fontSize: 20,
+  },
+  subtitleBox: {
+    width: '80%',
+    height: 150,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: '#2a1945',
+    marginTop: 30,
+    backgroundColor: '#311f4e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  whiteText: {
+    color: 'white'
+  },
+  textBox: {
+    width: '80%',
+    height: 430,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: '#2a1945',
+    marginTop: 30,
+    backgroundColor: '#311f4e',
+    alignItems: 'center',
   },
 });
