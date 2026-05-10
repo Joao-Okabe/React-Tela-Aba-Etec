a49d63a03ad6b2d095b05abe18a90281ac08a988
Joao-Okabe <joao.okae.berghem@gmail.com>
2026-05-10 13:37:24 -0300

    second and third year done

--- app/(tabs)/second_year.tsx
+++ app/(tabs)/second_year.tsx
@@ -2,18 +2,48 @@ import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <ScrollView
-      style={styles.fundo}
-      contentContainerStyle={styles.Container}
-    >
-      <Text 
-        style={styles.title}
+      <ScrollView
+        style={styles.fundo}
+        contentContainerStyle={styles.Container}
       >
-        Segundo Ano
-      </Text>
-      
-    </ScrollView>
-  );
+        <View
+          style={[styles.titleBox]}
+        >
+          <Text 
+            style={[styles.title, styles.whiteText]}
+          >
+            Segundo Ano
+          </Text>
+        </View>
+        
+        <View
+          style={[styles.subtitleBox]}
+        >
+          <Text
+            style={[styles.subtitle, styles.whiteText]}
+          >
+            Programador de Computadores
+          </Text>
+        </View>
+        <View
+          style={styles.textBox}
+        >
+          <Image
+            source={require('../../assets/images/phpbackend.png')}
+            style={{
+              width: 100,
+              aspectRatio: 0.8,
+            }}
+            resizeMode="contain"
+          />
+          <Text 
+            style={[styles.subtitle, styles.whiteText]}
+          >
+            Estudo de programação back-end 
+          </Text>
+        </View>
+      </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -23,11 +53,47 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   fundo: {
-    backgroundColor: 'white'
+    backgroundColor: '#263325'
   },
   title: {
     fontSize: 40,
-    marginTop: 40,
-    marginBottom: 80,
+  },
+  titleBox: {
+    width: '80%',
+    height: 150,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: '#112707',
+    marginTop: 30,
+    backgroundColor: '#16380a',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  subtitle: {
+    fontSize: 20,
+  },
+  subtitleBox: {
+    width: '80%',
+    height: 150,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: '#132c09',
+    marginTop: 30,
+    backgroundColor: '#19350f',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  whiteText: {
+    color: 'white'
+  },
+  textBox: {
+    width: '80%',
+    height: 430,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: '#0e2406',
+    marginTop: 30,
+    backgroundColor: '#133009',
+    alignItems: 'center',
   },
 });
--- app/(tabs)/third_year.tsx
+++ app/(tabs)/third_year.tsx
@@ -1,34 +1,104 @@
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <ScrollView
-      style={styles.fundo}
-      contentContainerStyle={styles.Container}
-    >
-      <Text 
-        style={styles.title}
+      <ScrollView
+        style={styles.fundo}
+        contentContainerStyle={styles.Container}
       >
-        Terceiro Ano
-      </Text>
-      
-      
-    </ScrollView>
-  );
+        <View
+          style={[styles.titleBox]}
+        >
+          <Text 
+            style={[styles.title, styles.whiteText]}
+          >
+            Terceiro Ano
+          </Text>
+        </View>
+        
+        <View
+          style={[styles.subtitleBox]}
+        >
+          <Text
+            style={[styles.subtitle, styles.whiteText]}
+          >
+            Técnico em 
+          </Text>
+          <Text
+            style={[styles.subtitle, styles.whiteText]}
+          >
+            Informática para Internet
+          </Text>
+        </View>
+        <View
+          style={styles.textBox}
+        >
+          <Image
+            source={require('../../assets/images/Google-flutter-logo.png')}
+            style={{
+              width: 100,
+              aspectRatio: 0.8,
+            }}
+            resizeMode="contain"
+          />
+          <Text 
+            style={[styles.subtitle, styles.whiteText]}
+          >
+            Estudo de desenvolvimento mobile 
+          </Text>
+        </View>
+      </ScrollView>
+    );
 }
 
-const styles = StyleSheet.create({  
+const styles = StyleSheet.create({
   Container: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
   },
   fundo: {
-    backgroundColor: 'white'
+    backgroundColor: '#ffacf7'
   },
   title: {
     fontSize: 40,
-    marginTop: 40,
-    marginBottom: 80,
+  },
+  titleBox: {
+    width: '80%',
+    height: 150,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: '#bb2a51',
+    marginTop: 30,
+    backgroundColor: '#d83c65',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  subtitle: {
+    fontSize: 20,
+  },
+  subtitleBox: {
+    width: '80%',
+    height: 150,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: '#a32446',
+    marginTop: 30,
+    backgroundColor: '#ba3055',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  whiteText: {
+    color: 'white'
+  },
+  textBox: {
+    width: '80%',
+    height: 430,
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: '#ba3055',
+    marginTop: 30,
+    backgroundColor: '#cd4d6f',
+    alignItems: 'center',
   },
 });
